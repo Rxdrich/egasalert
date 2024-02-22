24bce693ada1c602e521c9d6a82671be97de095a
Rxdrich <angelroro520@gmail.com>
2024-02-22 11:33:38 -0300

    First Commit

---
+++ src/main/java/com/angelrodriguez/egasalert/EgasalertApplication.java
@@ -0,0 +1,13 @@
+package com.angelrodriguez.egasalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgasalertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgasalertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/angelrodriguez/egasalert/EgasalertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelrodriguez.egasalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgasalertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
